406be0823363010e43305b7e0eb4cff91f5c7c08
Christian Agosto <christianagosto@Christians-Mac-mini.local>
2026-05-30 11:54:13 -0400

    modify the favicon and hide testimonial

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       <WhyChooseUs />
       <Gallery />
       <History />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <SocialFeed />
       <Contact />
       <Footer />
